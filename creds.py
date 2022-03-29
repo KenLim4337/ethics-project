3900d59c504602d02d0c67fd39a21089fdc778a2
KenLim4337 <lkyoong428@gmail.com>
2022-03-29 11:31:46 -0400

    separate creds from files

---
+++ creds.py
@@ -0,0 +1,29 @@
+#Twitter API Key
+def getTwitterAPI():
+    API_KEY = 'y9XmWFFQbpxhZ00aFlnm56Kiq'
+    return API_KEY
+
+#Twitter Secret
+def getTwitterSecret():
+    SECRET_KEY = 'SHc5tJ6WQJjjUISKHsU7b8SQXZwfRURsVmhnJZ5YEfTyRJzqCP'
+    return SECRET_KEY
+    
+#Twitter Bearer Token
+def getTwitterBearer():
+    BEARER = 'AAAAAAAAAAAAAAAAAAAAAId1awEAAAAApIzFwYQpDNSIn7qjKnbRtdgyf3Y%3D58pExLRCUMeVGf3MMCGp8N8QTJn1JZbvVnNartdh1TktBK1FWM'
+    return BEARER
+
+#Census API Key
+def getCensusAPI():
+    API_KEY = 'c9db62a0a80da33e1fa4a93c0d88cc65f9f08e72'
+    return API_KEY
+
+#SODA App Token
+def getSodaToken():
+    TOKEN = 'zHSUyA59WE3G9Zvypv4yLLdWH'
+    return TOKEN
+
+#SODA Secret Token
+def getSodaSecret():
+    SODA_SECRET = 'XrnRvmT16TO-uRl-5Kj__gjpKwpJbbHhZz2E'
+    return SODA_SECRET
---
+++ creds_template.py
@@ -0,0 +1,31 @@
+#Rename to creds.py after adding keys
+
+#Twitter API Key
+def getTwitterAPI():
+    API_KEY = ''
+    return API_KEY
+
+#Twitter Secret
+def getTwitterSecret():
+    SECRET_KEY = ''
+    return SECRET_KEY
+    
+#Twitter Bearer Token
+def getTwitterBearer():
+    BEARER = ''
+    return BEARER
+
+#Census API Key
+def getCensusAPI():
+    API_KEY = ''
+    return API_KEY
+
+#SODA App Token
+def getSodaToken():
+    TOKEN = ''
+    return TOKEN
+
+#SODA Secret Token
+def getSodaSecret():
+    SODA_SECRET = ''
+    return SODA_SECRET
