54d3f70c052a0fa2caa3bca6193fb662c74c76db
taonyzeng <52616901+taonyzeng@users.noreply.github.com>
2022-03-31 11:10:56 -0400

    added the cred for asc

---
+++ creds.py
@@ -0,0 +1,31 @@
+#Rename to creds.py after adding keys
+
+#Twitter API Key
+def getTwitterAPI():
+    API_KEY = ''
+    return API_KEY
+
+#Twitter Secret
+def getTwitterSecret():
+    SECRET_KEY = ''
+    return SECRET_KEY
+    
+#Twitter Bearer Token
+def getTwitterBearer():
+    BEARER = ''
+    return BEARER
+
+#Census API Key
+def getCensusAPI():
+    API_KEY = '268949463634955799b3cc2da21a4cf457510677'
+    return API_KEY
+
+#SODA App Token
+def getSodaToken():
+    TOKEN = ''
+    return TOKEN
+
+#SODA Secret Token
+def getSodaSecret():
+    SODA_SECRET = ''
+    return SODA_SECRET
